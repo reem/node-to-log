a4e9ac89090a2875bd73d22576c99e21df47138a
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-01 19:04:39 -0700

    (doc) Added example.

--- examples/example.js
+++ examples/example.js
@@ -1,3 +1,11 @@
 var toLog = require('to-log');
+var fs = require('fs');
 
-toLog.doSomethingCool();
+toLog({
+  stdout: fs.createWriteStream('your-log-file.txt'),
+  stderr: fs.createWriteStream('your-error-file.txt')
+});
+
+console.log("Hello!"); // Logged to your-log-file.txt
+console.error("Something bad happened!"); // Logged to your-error-file.txt
+
